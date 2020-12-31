9a389fe13af9766bc3c5abe2161e079e56997ab5
Guillermo Espert <gespert@yahoo.es>
2020-12-31 13:36:01 +0100

    Servidor de actualizaciones
    Carga del servidor de actualizaciones para LiteDB Manager

---
+++ LdbMUpdateServer/main.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<h1>Servidor de actualizaciones</h1><p>El servidor de actualizaciones está operativo</p>');
+})
+
+app.get('/lastversion', function(req, res) {
+    res.json({
+        version : "0.1",
+        downloadUrl : "http://localhost:8085/downloads/test.txt"
+    })
+})
+
+app.get('/downloads/:document', function(req, res) {
+    const fileName = req.params.document;
+    const directoryPath = __dirname + "/downloads/";
+  
+    res.download(directoryPath + fileName, fileName, (err) => {
+      if (err) {
+        res.status(500).send({
+          message: "No se puede descargar el archivo. " + err,
+        })
+      }
+    })
+})
+
+var server = app.listen(8085, function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Servidor activo en http://%s:%s', host, port)
+})
